0ac2183290704b0267896142440b0b90948953bb
Tim <timdrudy@gmail.com>
2025-03-26 16:31:59 -0500

    added update post button and more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase/init';
+import { auth, db } from './firebase/init';
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc } from "firebase/firestore"
 import { 
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword, 
@@ -11,6 +12,47 @@ onAuthStateChanged,
 function App() {
   const [user,setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
+
+  async function updatePost() {
+    const hardcodedId = "1H6tpHoqTSoMoHcwIdIx"
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId);
+    const newPost = {
+      ...post,
+      title: "Land a 400k job"
+    };
+    console.log(newPost);
+    updateDoc(postRef, newPost)
+  }
+
+  function createPost() {
+    const post = {
+      title: "Finish Firebase Section",
+      description: "do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post)
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, "posts"));
+    const posts = docs.map((elem) => ({...elem.data(), id: elem.id }));
+  }
+
+  async function getPostById(id) {
+    const postRef = doc(db, "posts", id);
+    const postSnap = await getDoc(postRef);
+    return postSnap.data();
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs.map(doc => doc.data()));
+  }
 
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -50,13 +92,15 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={register} className='btn_purple'>Register</button>
-
-      <button onClick={login} className='btn'>Login</button>
-
-      <button onClick={logout} className='btn'>Logout</button>
-
-      {loading ? 'loading...' : <button className='btn_purple'> {user.email[0].toUpperCase()} </button>}
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...' :  user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By ID</button>
+      <button onClick={getPostByUid}>Get Post By UID</button>
+      <button onClick={updatePost}>Update Post</button>
     </div>
   );
 }
